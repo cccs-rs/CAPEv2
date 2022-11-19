65c7b5fe88338446a6556e73d186f1bcff9d4152
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2022-11-19 03:44:25 -0500

    Add yara rule to TSCookie

--- modules/processing/parsers/CAPE/TSCookie.py
+++ modules/processing/parsers/CAPE/TSCookie.py
@@ -14,6 +14,21 @@ import pefile
 
 DESCRIPTION = "TSCookie configuration parser."
 AUTHOR = "JPCERT"
+rule_source = """
+rule TSCookie
+{
+    meta:
+        author = "kevoreilly"
+        description = "TSCookie Payload"
+        cape_type = "TSCookie Payload"
+    strings:
+        $string1 = "http://%s:%d" wide
+        $string2 = "/Default.aspx" wide
+        $string3 = "\\wship6"
+    condition:
+        uint16(0) == 0x5A4D and all of them
+}
+"""
 
 # Resource pattern
 RESOURCE_PATTERNS = [
