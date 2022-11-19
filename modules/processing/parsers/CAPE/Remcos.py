b22535553f691cffb4fbc4099c7e254c96858a6c
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2022-11-19 10:13:06 -0500

    Add yara rule to remcos parser

--- modules/processing/parsers/CAPE/Remcos.py
+++ modules/processing/parsers/CAPE/Remcos.py
@@ -19,6 +19,22 @@ from Cryptodome.Cipher import ARC4
 DESCRIPTION = "Remcos config extractor."
 AUTHOR = "threathive,sysopfb,kevoreilly"
 
+rule_source = """
+rule Remcos
+{
+    meta:
+        author = "kevoreilly"
+        description = "Remcos Payload"
+        cape_type = "Remcos Payload"
+    strings:
+        $name  = "Remcos" nocase
+        $time   = "%02i:%02i:%02i:%03i"
+        $crypto1 = {81 E1 FF 00 00 80 79 ?? 4? 81 C9 00 FF FF FF 4? 8A ?4 8?}
+        $crypto2 = {0F B6 [1-7] 8B 45 08 [0-2] 8D 34 07 8B 01 03 C2 8B CB 99 F7 F9 8A 84 95 ?? ?? FF FF 30 06 47 3B 7D 0C 72}
+    condition:
+        uint16(0) == 0x5A4D and ($name) and ($time) and any of ($crypto*)
+}
+"""
 
 # From JPCERT
 FLAG = {b"\x00": "Disabled", b"\x01": "Enabled"}
