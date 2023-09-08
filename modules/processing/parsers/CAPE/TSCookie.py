76050303cd35e94e29b058c3abb9fe15c9175eaf
cccs-rs <ryan.samaroo@cyber.gc.ca>
2023-09-08 14:12:01 +0000

    Add yara rules to corresponding parsers

--- modules/processing/parsers/CAPE/TSCookie.py
+++ modules/processing/parsers/CAPE/TSCookie.py
@@ -24,7 +24,7 @@ rule TSCookie
     strings:
         $string1 = "http://%s:%d" wide
         $string2 = "/Default.aspx" wide
-        $string3 = "\\wship6"
+        $string3 = "\\\\wship6"
     condition:
         uint16(0) == 0x5A4D and all of them
 }
